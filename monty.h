e65eaa1eca8678f6fd5ca657810466520da9c454
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-14 21:19:50 +0100

    fixe

--- monty.h
+++ monty.h
@@ -52,5 +52,6 @@ void free_stack(stack_t *stack);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
 /*-------------------------------PROTOTYPES-------------------------------*/
 #endif
--- opcode.c
+++ opcode.c
@@ -14,6 +14,7 @@ void (*check_instruction(char *command))(stack_t**, unsigned int)
 		{"pall", pall},
 		{"pint", pint},
 		{"pop", pop},
+		{"swap", swap},
 		{"nop", nop},
 		{NULL, NULL}
 	};
