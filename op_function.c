5513a1791a0c6c3f9178afcbce5b56b4a493aee8
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-16 23:03:07 +0100

    op_function.c

--- op_function.c
+++ op_function.c
@@ -78,35 +78,3 @@ void pchar(stack_t **stack, unsigned int line_number)
 	}
 }
 
-/**
- *pstr -prints the string starting at the top of the stack
- *
- *@stack: pointer of struct
- *@line_number: cont the number of ech line
- *
- */
-void pstr(stack_t **stack, unsigned int line_number)
-{
-
-(void)line_number;
-stack_t *tmp = *stack;
-		if (*stack == NULL || stack == NULL)
-		{
-			putchar('\n');
-			return;
-		}
-		else
-		{
-			while (tmp)
-			{
-				if (tmp->n > 127 || tmp->n <= 0)
-					break;
-
-			putchar(tmp->n);
-			tmp = tmp->next;
-			}
-
-			putchar('\n');
-		}
-
-}
