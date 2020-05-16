0ea06368d1cf8fdf576874a3325eb269b01d4721
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-16 23:25:06 +0100

    pstr

---
+++ extra_func.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ *pstr -prints the string starting at the top of the stack
+ *
+ *@stack: pointer of struct
+ *@line_number: cont the number of ech line
+ *
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+stack_t *tmp = *stack;
+(void)line_number;
+
+	if (*stack == NULL || stack == NULL)
+	{
+		putchar('\n');
+		return;
+	}
+
+	else
+	{
+		while (tmp)
+		{
+			if (tmp->n > 127 || tmp->n <= 0)
+				break;
+
+			putchar(tmp->n);
+			tmp = tmp->next;
+		}
+
+		putchar('\n');
+	}
+
+}
