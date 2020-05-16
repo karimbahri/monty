16e74fa1969d9e58de10453210b4a2150277a4f5
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-16 22:28:28 +0100

    pstr

--- op_function.c
+++ op_function.c
@@ -61,7 +61,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 
 		if (*stack == NULL || stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pchar an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
@@ -78,3 +78,31 @@ void pchar(stack_t **stack, unsigned int line_number)
 	}
 }
 
+/**
+ *pstr -prints the string starting at the top of the stack
+ *
+ *@stack: pointer of struct
+ *@line_number: cont the number of ech line
+ *
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+
+(void)line_number;
+		if (*stack == NULL || stack == NULL)
+			putchar('\n');
+		else
+		{
+			while (*stack)
+			{
+				if ((*stack)->n > 127 || (*stack)->n <= 0)
+					break;
+
+			putchar((*stack)->n);
+			*stack = (*stack)->next;
+			}
+
+			putchar('\n');
+		}
+
+}
